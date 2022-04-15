aa494ce1971f8866418c2811f8ee2de5c74038e2
ibraheemsag2 <ibraheem@live.unc.edu>
2022-04-15 16:52:42 -0400

    shit should be good

--- flips.js
+++ flips.js
@@ -1,7 +1,7 @@
 import * as coin from './modules/coin.mjs';
-import {createRequire} from "module";
-const args = require('minimist')(process.argv.slice(2))
-const num = args.number || 1;
+import minimist from "minimist";
+var arg = minimist(process.argv.slice(2));
+
+const num = arg["number"] || 1;
 const array = coin.coinFlips(num);
-console.log(array);
 console.log(coin.countFlips(array));
