6cc68257ef84dbf755a519d47091da8a609dd616
ibraheemsag2 <ibraheem@live.unc.edu>
2022-04-15 17:30:48 -0400

    Error testing

--- guess-flip.js
+++ guess-flip.js
@@ -3,8 +3,12 @@ import minimist from "minimist";
 var arg = minimist(process.argv.slice(2));
 if(arg["call"] == "heads" || arg["call"] == "tails"){
     console.log(coin.flipACoin(arg["call"]))}
-else{
+else if(arg["call"] == null){
     console.error("Error: no input.")
+    console.error("Usage: node guess-flip --call=[heads|tails]")
+    process.exit();
+}
+else{
     console.error("Usage: node guess-flip --call=[heads|tails]")
     process.exit();
 }
