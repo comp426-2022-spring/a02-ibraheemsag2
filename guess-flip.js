97eba407fcc325384c18f73b7cb4cfaa09bdc41f
ibraheemsag2 <ibraheem@live.unc.edu>
2022-04-15 16:52:14 -0400

    Shit should be good now

--- guess-flip.js
+++ guess-flip.js
@@ -1,5 +1,7 @@
 import * as coin from './modules/coin.mjs';
-const args = require('minimist')(process.argv.slice(2))
+import minimist from "minimist";
+var arg = minimist(process.argv.slice(2));
+
 const calls = args.call
 console.log(coin.flipACoin(calls))
 
